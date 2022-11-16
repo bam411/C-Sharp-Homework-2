feca76b772e9911a9c8482ad955591fa32c03e92
Petrushov Denis <bam411@yandex.ru>
2022-11-16 12:33:10 +0300

    exercise 10

---
+++ exercise_10/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int divideNumber = number / 10;
+int secondDigit = divideNumber % 10;
+
+Console.WriteLine(number);
+Console.WriteLine(secondDigit);
